8dd9b106d2d824f3ae2e79437f40eca8e74f1035
aniuxa <ana.escoto@politicas.unam.mx>
2021-08-10 16:09:51 -0500

    Código modificado Histograma de mpg

--- R/Ana.R
+++ R/Ana.R
@@ -6,3 +6,9 @@ data("mtcars")
 head(mtcars)
 
 hist(mtcars$mpg)
+
+pdf("graficas/mtcars_arec.pdf") 
+ggplot(mtcars, aes(x = mpg)) + 
+  geom_histogram(fill = "cyan4", color = "black") + theme_classic() +
+  xlab("MPG") + ggtitle("MTCARS")
+dev.off()
