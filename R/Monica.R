fc8916ef9bf751007f94509e922e922d6d73698f
mruizros <mruizros@gmail.com>
2021-08-10 16:23:08 -0500

    cambio

--- R/Monica.R
+++ R/Monica.R
@@ -2,12 +2,11 @@ library('ggplot2')
 
 data <- data.frame(mtcars)
 
-pdf("graficas/CilindrajeDeCarros.pdf") 
 
 ggplot(data=data, aes(x=cyl, y=row.names(data))) +
   geom_bar(stat="identity")
 
 
-dev.off()
+ggsave("graficas/CilindrajeDeCarros.pdf")
 
 
